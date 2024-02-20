b2d8f6e5a35942f1022f1d54fd70a85460251203
codetron21 <87284524+codetron21@users.noreply.github.com>
2024-02-20 16:13:28 +0700

    update

---
+++ src/Applications/use_case/_test/AddCommentUseCase.test.js
@@ -0,0 +1,4 @@
+
+describe('AddCommentUseCase', () => {
+
+});
--- src/Applications/use_case/_test/GetDetailThreadUseCase.test.js
+++ src/Applications/use_case/_test/GetDetailThreadUseCase.test.js
@@ -1,5 +1,8 @@
-
+const GetDetailThreadUseCase = require('../GetDetailThreadUseCase');
+const ThreadRepository = require("../../../Domains/threads/ThreadRepository");
 
 describe('GetDetailThreadUseCase', () => {
+    it('should throw error if use case payload not contain refresh token', async () => {
 
+    });
 });
