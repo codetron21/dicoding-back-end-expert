bd8b9c6225881cca5a1530b064e77e3a856483a6
codetron21 <codetronclan@gmail.com>
2024-02-21 07:55:55 +0700

    update

--- src/Applications/use_case/_test/DeleteCommentUseCase.test.js
+++ src/Applications/use_case/_test/DeleteCommentUseCase.test.js
@@ -1,5 +1,55 @@
-
+const AuthenticationTokenManager = require('../../security/AuthenticationTokenManager');
+const ThreadRepository = require("../../../Domains/threads/ThreadRepository");
+const CommentRepository = require("../../../Domains/comments/CommentRepository");
+const DeleteCommentUseCase = require('../DeleteCommentUseCase');
 
 describe('DeleteCommentUseCase', () => {
+    it('should throw error when access token not attached', async () => {
+        // Arrange
+        const accessToken = null;
 
+        const useCase = new DeleteCommentUseCase({});
+
+        // Act & Assert
+        await expect(useCase.execute(null, accessToken))
+            .rejects
+            .toThrowError('DELETE_COMMENT_USE_CASE.NOT_CONTAIN_ACCESS_TOKEN');
+    });
+
+    it('should pass only token to a function', async () => {
+        // Arrange
+        const accessToken = 'Bearer token';
+        const token = 'token';
+        const mockTokenManager = new AuthenticationTokenManager();
+        const mockThreadRepository = new ThreadRepository();
+        const mockCommentRepository = new CommentRepository();
+
+        mockTokenManager.verifyAccessToken = jest.fn()
+            .mockImplementation(() => Promise.resolve());
+        mockTokenManager.decodePayload = jest.fn()
+            .mockImplementation(() => Promise.resolve({id: 1}));
+        mockThreadRepository.checkAvailabilityThread = jest.fn()
+            .mockImplementation(() => Promise.resolve());
+        mockCommentRepository.checkAvailabilityComment = jest.fn()
+            .mockImplementation(() => Promise.resolve());
+        mockCommentRepository.verifyCommentOwner = jest.fn()
+            .mockImplementation(() => Promise.resolve());
+        mockCommentRepository.removeComment = jest.fn()
+            .mockImplementation(() => Promise.resolve());
+
+        const useCase = new DeleteCommentUseCase({
+            authenticationTokenManager: mockTokenManager,
+            commentRepository: mockCommentRepository,
+            threadRepository: mockThreadRepository
+        });
+
+        // Act
+        await useCase.execute({}, accessToken);
+
+        // Assert
+        expect(mockTokenManager.verifyAccessToken)
+            .toBeCalledWith(token);
+        expect(mockTokenManager.decodePayload)
+            .toBeCalledWith(token);
+    });
 });
--- src/Applications/use_case/_test/GetDetailThreadUseCase.test.js
+++ src/Applications/use_case/_test/GetDetailThreadUseCase.test.js
@@ -1,8 +1,53 @@
 const GetDetailThreadUseCase = require('../GetDetailThreadUseCase');
 const ThreadRepository = require("../../../Domains/threads/ThreadRepository");
+const CommentRepository = require("../../../Domains/comments/CommentRepository");
 
 describe('GetDetailThreadUseCase', () => {
-    it('should throw error if use case payload not contain refresh token', async () => {
+    it('should return data with valid data', async () => {
+        // Arrange
+        const mockedThreadData = {
+            "status": "success",
+            "data": {
+                "thread": {
+                    "id": "thread-h_2FkLZhtgBKY2kh4CC02",
+                    "title": "sebuah thread",
+                    "body": "sebuah body thread",
+                    "date": "2021-08-08T07:19:09.775Z",
+                    "username": "dicoding",
+                }
+            }
+        };
 
+        const mockedComment = [
+            {
+                "id": "comment-yksuCoxM2s4MMrZJO-qVD",
+                "username": "dicoding",
+                "date": "2021-08-08T07:26:21.338Z",
+                "content": "**komentar telah dihapus**"
+            }
+        ];
+
+        const mockThreadRepository = new ThreadRepository();
+        const mockCommentRepository = new CommentRepository();
+
+        mockThreadRepository.getDetailThread = jest.fn()
+            .mockImplementation(() => Promise.resolve(mockedThreadData));
+
+        mockCommentRepository.getCommentsByThreadId = jest.fn()
+            .mockImplementation(() => Promise.resolve(mockedComment));
+
+        const useCase = new GetDetailThreadUseCase({threadRepository:mockThreadRepository,commentRepository: mockCommentRepository});
+
+        const id = '1';
+
+        // Act
+        const result = await useCase.execute(id);
+
+        // assert
+        expect(mockThreadRepository.getDetailThread)
+            .toBeCalledWith(id);
+        expect(mockCommentRepository.getCommentsByThreadId)
+            .toBeCalledWith(id);
+        expect(result.comments[0].content).toEqual("**komentar telah dihapus**");
     });
 });
